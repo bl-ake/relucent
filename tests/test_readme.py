e5bd04ee55830e53a8cb4a717890987f55543f1f
blake <blake@uconn.edu>
2026-04-01 16:59:28 -0400

    wrap test

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -5,31 +5,33 @@ import torch.nn as nn
 
 import relucent
 
-# Create Model
-network = nn.Sequential(
-    nn.Linear(2, 10),
-    nn.ReLU(),
-    nn.Linear(10, 5),
-    nn.ReLU(),
-    nn.Linear(5, 1),
-)  ## or conveniently, relucent.get_mlp_model(widths=[2, 10, 5, 1])
 
-## Initialize a Complex to track calculations
-cplx = relucent.Complex(network)
+def test_readme():
+    # Create Model
+    network = nn.Sequential(
+        nn.Linear(2, 10),
+        nn.ReLU(),
+        nn.Linear(10, 5),
+        nn.ReLU(),
+        nn.Linear(5, 1),
+    )  ## or conveniently, relucent.get_mlp_model(widths=[2, 10, 5, 1])
 
-## Calculate the activation regions via local search
-if __name__ == "__main__":
+    ## Initialize a Complex to track calculations
+    cplx = relucent.Complex(network)
+
+    ## Calculate the activation regions via local search
     cplx.bfs()
 
-## Plotting functions return Plotly figures
-fig = cplx.plot_cells()
+    ## Plotting functions return Plotly figures
+    _ = cplx.plot_cells()
 
-input_point = np.random.random((1, 2))
-p = cplx.point2poly(input_point)
+    assert len(cplx) > 0
+    assert sum(len(p.shis) for p in cplx) / len(cplx) > 0
 
-_ = sum(len(p.shis) for p in cplx) / len(cplx)
+    input_point = np.random.random((1, 2))
+    p = cplx.point2poly(input_point)
 
-_ = p.halfspaces[p.shis]
-_ = p.center
-_ = p.inradius
-_ = cplx.get_dual_graph()
+    _ = p.halfspaces[p.shis]
+    _ = p.center
+    _ = p.inradius
+    _ = cplx.get_dual_graph()
